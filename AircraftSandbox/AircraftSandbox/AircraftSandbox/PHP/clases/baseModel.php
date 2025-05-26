9aca5f13167febe1f761f7ced9cfb20acf9be2d3
TatkovOleksandr <zmeelov2015@email.com>
2025-05-26 12:45:40 +0300

    fix lb3 smp

--- AircraftSandbox/AircraftSandbox/AircraftSandbox/PHP/clases/baseModel.php
+++ AircraftSandbox/AircraftSandbox/AircraftSandbox/PHP/clases/baseModel.php
@@ -19,5 +19,41 @@ class BaseModel {
             throw $e;
         }
     }
+
+    private function init(): void
+    {
+        try {
+            $this->db->beginTransaction();
+
+            $this->db->exec("
+                CREATE TABLE IF NOT EXISTS User (
+                  Login TEXT PRIMARY KEY,
+                  Name TEXT,
+                  Password TEXT,
+                  Phone TEXT,
+                  IsSuperUser INTEGER
+                );
+            ");
+            $this->db->exec("
+                CREATE TABLE IF NOT EXISTS Post (
+                  Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  Header TEXT,
+                  ImagePath TEXT,
+                  Content TEXT,
+                  LikesCount INTEGER,
+                  DislikesCount INTEGER,
+                  ownerLogin TEXT,
+                  FOREIGN KEY(ownerLogin) REFERENCES User(Login)
+                );
+            ");
+
+            $this->db->exec("INSERT OR IGNORE INTO User (Login,Name,Password,Phone,IsSuperUser)
+                             VALUES ('admin','Admin','', '0', 1);");
+            $this->db->commit();
+        } catch (PDOException $e) {
+            $this->db->rollBack();
+            exit('Не вдалося ініціалізувати БД: ' . htmlspecialchars($e->getMessage()));
+        }
+    }
 }
 ?>
