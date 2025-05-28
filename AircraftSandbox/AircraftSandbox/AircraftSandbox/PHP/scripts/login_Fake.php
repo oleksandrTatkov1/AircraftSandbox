fe22f5630c340aafb1824a543e9baa6cb6a453c0
EnotskyOcheret <fourteateogmail.com>
2025-05-28 21:22:22 +0300

    class user + profil huynya

--- AircraftSandbox/AircraftSandbox/AircraftSandbox/PHP/scripts/login_Fake.php
+++ AircraftSandbox/AircraftSandbox/AircraftSandbox/PHP/scripts/login_Fake.php
@@ -1,18 +1,37 @@
 <?php
-// php/api/sessionProfile.php
+
 session_start();
 
-// Фейковий користувач (імітація після авторизації)
+require_once __DIR__ . '/../clases/User.php';
+use PHP\Clases\User;
+
+// Фейковий користувач
 $_SESSION['user'] = [
-    'Login' => 'glory@aviation.com',
+    'Login' => 'huybobra@gmail.com',
     'Name' => 'FimoznikPetya',
-    'Password' = 'huesos228',
-    'Phone' = '+380935262394',
-    'IsSuperUser' = 0,
-    'ImagePath' => '"/xampp/img/fimozphoto.jpg"',
+    'Password' => 'huesos228',
+    'Phone' => '+380935262394',
+    'IsSuperUser' => 0,
+    'ImagePath' => '/xampp/img/fimozphoto.jpg',
     'Bio' => 'Ебашу пятилеток'
 ];
 
-// Видача у форматі JSON
+// Створення об'єкта User з даних сесії
+$user = new User();
+$user->Login = $_SESSION['user']['Login'];
+$user->Name = $_SESSION['user']['Name'];
+$user->Password = $_SESSION['user']['Password'];
+$user->Phone = $_SESSION['user']['Phone'];
+$user->IsSuperUser = $_SESSION['user']['IsSuperUser'];
+$user->ImagePath = $_SESSION['user']['ImagePath'];
+$user->Bio = $_SESSION['user']['Bio'];
+
+// Вивід JSON
 header('Content-Type: application/json');
-echo json_encode($_SESSION['user']);
+echo json_encode([
+    'Login' => $user->Login,
+    'Name' => $user->Name,
+    'Phone' => $user->Phone,
+    'ImagePath' => $user->ImagePath,
+    'Bio' => $user->Bio
+]);
